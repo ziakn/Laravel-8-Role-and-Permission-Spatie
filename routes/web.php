efa90d924e0de69d8a9092846559d783ae78a78d
ziakn <ziakn03@gmail.com>
2021-04-06 09:18:23 +0300

    Role And Permission Setup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
-
+  
 use Illuminate\Support\Facades\Route;
-
+  
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
+  
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +17,17 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+  
 Route::get('/', function () {
     return view('welcome');
+});
+  
+Auth::routes();
+  
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+  
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
+    Route::resource('products', ProductController::class);
 });
